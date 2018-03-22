ac751fa2f49cb4f36e0c5629c8b5a86d8e610c2e
Amimaro <amir.zahlan@gmail.com>
2018-03-21 21:13:11 -0300

    Created blocks and board

--- src/App.js
+++ src/App.js
@@ -2,17 +2,61 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Block extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        block: ['red', 'blue']
+    }
+  }
+  render() {
+    return (
+      <span style={{ backgroundColor: this.state.block[this.props.value] }}>&nbsp;&nbsp;</span>
+    );
+  }
+}
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        board: Array(15000).fill(0)
+    }
+    this.renderBoard = this.renderBoard.bind(this);
+    this.renderBlock = this.renderBlock.bind(this);
+  }
+  renderBoard() {
+    let rows = [];
+    this.state.board.map((block, index) => {
+      if(index === 10)
+        rows.push(this.renderBlock(1));
+      else if(index % 200 === 0)
+        rows.push(<br/>);
+      else
+        rows.push(this.renderBlock(block));
+    })
+    return rows;
+  }
+  renderBlock(code) {
+    return <Block value={code} />;
+  }
+  render() {
+    return (
+      <div className="board">
+        {this.renderBoard()}
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Roguelike Dungeon Crawler</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Map />
       </div>
     );
   }
