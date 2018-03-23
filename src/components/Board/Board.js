a84d7d4c24da92cd3e231ead36f69827a4ee529e
Amimaro <amir.zahlan@gmail.com>
2018-03-23 15:42:34 -0300

    Detect arrow keys

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -7,31 +7,30 @@ class Board extends Component {
     super(props);
     this.state = {
         renderedBoard: [],
-        board: Array(parseInt(this.props.TOTAL_BLOCKS)).fill(0),
+        board: Array(parseInt(this.props.TOTAL_BLOCKS, 10)).fill(0),
         rows: this.props.TOTAL_BLOCKS / this.props.WIDTH * this.props.BLOCK_SIZE,
         columns: this.props.TOTAL_BLOCKS / this.props.HEIGHT * this.props.BLOCK_SIZE
     }
-    this.renderBoard = this.renderBoard.bind(this);
-    this.renderBoard = this.renderBoard.bind(this);
-    this.generateDungeons = this.generateDungeons.bind(this);
     this.getRow = this.getRow.bind(this);
     this.getColumn = this.getColumn.bind(this);
-    this.createRect = this.createRect.bind(this);
     this.countWalls = this.countWalls.bind(this);
     this.getRand = this.getRand.bind(this);
+    this.generateDungeons = this.generateDungeons.bind(this);
+    this.renderBoard = this.renderBoard.bind(this);
+    this.createRect = this.createRect.bind(this);
     this.createRandomPath = this.createRandomPath.bind(this);
     this.createElements = this.createElements.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
   componentWillMount() {
     this.generateDungeons();
+    document.addEventListener("keydown", this.handleKeyPress, false);
   }
-  componentDidMount() {
-    this.renderBoard();
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.handleKeyPress, false);
   }
   generateDungeons() {
-    let board = Array(parseInt(this.props.TOTAL_BLOCKS)).fill(0);
-    let rows = this.state.rows;
-    let columns = this.state.columns;
+    let board = Array(parseInt(this.props.TOTAL_BLOCKS, 10)).fill(0);
 
     for(let i = 5; i < 99; i+=20) {
       board = this.createRandomPath(board, i, 'horizontal');
@@ -56,15 +55,17 @@ class Board extends Component {
     // Player
     board = this.createElements(board, [0, 0, 0, 0, 0.01], [40, 40, 40, 40, 40], 1);
 
-    this.setState({board: board});
+    let renderBoard = this.renderBoard(board);
+
+    this.setState({board: board, renderedBoard: renderBoard});
   }
   createRect(board, x0, y0, x1, y1) {
     return board.map((block, index) => {
       if(this.getRow(index) >= x0 && this.getRow(index) <= x1 &&
          this.getColumn(index) >= y0 && this.getColumn(index) <= y1 &&
-         board[index] == 0){
+         board[index] === 0){
           return 1;
-      } else if(board[index] == 1) {
+      } else if(board[index] === 1) {
         return 1;
       } else {
         return 0;
@@ -75,22 +76,22 @@ class Board extends Component {
     let turn = false; // false - turn left; true - turn right
     let position = 0;
     for(let i = 10; i < 90; i++) { // offset borders
-      if(direction == 'horizontal')
+      if(direction === 'horizontal')
         position = i + 100 * pivot;
-      else if(direction == 'vertical')
+      else if(direction === 'vertical')
         position = pivot + 100 * i;
       board[position] = 1;
       if(this.getRand(0,10) > 8) { // 20% chance to turn to a direction
         for(let j = 0; j < this.getRand(3,5); j++) { // walk up and down
-          turn == true ? pivot++ : pivot--;
-          if(direction == 'horizontal') {
+          turn === true ? pivot++ : pivot--;
+          if(direction === 'horizontal') {
             position = i + 100 * pivot;
-            if(this.getRow(position) == 0)
+            if(this.getRow(position) === 0)
               pivot++;
             position = i + 100 * pivot;
-          } else if(direction == 'vertical'){
+          } else if(direction === 'vertical'){
             position = pivot + 100 * i;
-            if(this.getColumn(position) == 0)
+            if(this.getColumn(position) === 0)
               pivot++;
             position = pivot + 100 * i;
           }
@@ -105,36 +106,35 @@ class Board extends Component {
     let count = 0;
     return board.map((block, index) => {
       let row = this.getRow(index);
-      let column = this.getColumn(index);
-      if(quantity == count) {
+      if(quantity === count) {
         return board[index];
       }
       if(row < 20) {
-        if(board[index] == 1 && Math.random() <= chance[0]){
+        if(board[index] === 1 && Math.random() <= chance[0]){
           if(quantity > 0)
             count++;
           return element[0];
         }
       } else if(row >= 20 && row < 40){
-        if(board[index] == 1 && Math.random() <= chance[1]){
+        if(board[index] === 1 && Math.random() <= chance[1]){
           if(quantity > 0)
             count++;
           return element[1];
         }
       } else if(row >= 40 && row < 60) {
-        if(board[index] == 1 && Math.random() <= chance[2]){
+        if(board[index] === 1 && Math.random() <= chance[2]){
           if(quantity > 0)
             count++;
           return element[2];
         }
       } else if(row >= 60 && row < 80) {
-        if(board[index] == 1 && Math.random() <= chance[3]){
+        if(board[index] === 1 && Math.random() <= chance[3]){
           if(quantity > 0)
             count++;
           return element[3];
         }
       } else if(row >= 80) {
-        if(board[index] == 1 && Math.random() <= chance[4]){
+        if(board[index] === 1 && Math.random() <= chance[4]){
           if(quantity > 0)
             count++;
           return element[4];
@@ -150,17 +150,17 @@ class Board extends Component {
       count ++;
     if(board[index-1] === 0 && column > 0)
       count ++;
-    if(board[index-100] == 0 && row > 0)
+    if(board[index-100] === 0 && row > 0)
       count ++;
-    if(board[index+100] == 0 && row < 99)
+    if(board[index+100] === 0 && row < 99)
       count ++;
-    if(board[index+99] == 0 && row < 99 && column > 0)
+    if(board[index+99] === 0 && row < 99 && column > 0)
       count ++;
-    if(board[index-99] == 0 && row > 0 && column < 99)
+    if(board[index-99] === 0 && row > 0 && column < 99)
       count ++;
-    if(board[index-101] == 0 && column > 0 && row > 0)
+    if(board[index-101] === 0 && column > 0 && row > 0)
       count ++;
-    if(board[index+101] == 0 && column < 99 && row < 99)
+    if(board[index+101] === 0 && column < 99 && row < 99)
       count ++;
     return count;
   }
@@ -176,15 +176,33 @@ class Board extends Component {
   renderBlock(block) {
     return <Block value={block.value} key={block.index} index={block.index} walls={this.countWalls(this.state.board, block.index)} />;
   }
-  renderBoard() {
-    let renderedBoard = this.state.board.map((block, index) => {
+  renderBoard(board) {
+    let renderedBoard = board.map((block, index) => {
         return this.renderBlock({value: block, index: index});
     });
-    this.setState({renderedBoard: renderedBoard});
+    return renderedBoard;
+  }
+  handleKeyPress = (event) => {
+    switch(event.keyCode){
+      case 37:
+        console.log('left');
+      break;
+      case 38:
+        console.log('up');
+      break;
+      case 39:
+        console.log('right');
+      break;
+      case 40:
+        console.log('down');
+      break;
+    }
   }
   render() {
     return (
-        <div className="board" style={{ width: this.props.WIDTH + 'px', height: this.props.HEIGHT + 'px' }}>
+        <div className="board"
+             style={{ width: this.props.WIDTH + 'px', height: this.props.HEIGHT + 'px' }}
+             onKeyDown={this.handleKeyPress}>
           {this.state.renderedBoard}
         </div>
     );
