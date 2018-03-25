f2d4b8fb916803e5475f390ea479defb7333bbb3
Amimaro <amir.zahlan@gmail.com>
2018-03-25 17:07:21 -0300

    Add status

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Board from './components/Board/Board';
 import Listener from './components/Listener/Listener';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -12,7 +13,11 @@ class App extends Component {
             src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" alt="Fork me on GitHub" />
         </a>
         <header className="App-header">
-          <h1 className="App-title">React Roguelike Dungeon Crawler</h1>
+          <p>
+            <a href="https://www.freecodecamp.org/challenges/build-a-roguelike-dungeon-crawler-game" target="_blank">
+              React Roguelike Dungeon Crawler
+            </a>
+          </p>
           <p>by <a href="https://github.com/amimaro" target="_blank">amimaro</a></p>
         </header>
         <Listener></Listener>
@@ -21,15 +26,14 @@ class App extends Component {
             <Board TOTAL_BLOCKS="10000" WIDTH="1000" HEIGHT="1000" BLOCK_SIZE="10"/>
           </div>
         </div>
-        <footer className="App-footer">
-          <p>
-            <strong>
-              <a href="https://github.com/amimaro/dungeon-crawler">
-                dungeon-crawler
-              </a>
-            </strong>. The source code is licensed MIT.
-          </p>
-        </footer>
+        <div className="status">
+          <h4 style={{color: 'black'}}>Level: 0</h4>
+          <h4 style={{color: 'green'}}>HP: 100</h4>
+          <h4 style={{color: 'black'}}>Weapon: stick</h4>
+          <h4 style={{color: 'red'}}>Atack: 7</h4>
+          <p><Button>Restart</Button></p>
+          <p><Button>Toggle Darkness</Button></p>
+        </div>
       </div>
     );
   }
