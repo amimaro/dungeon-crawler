98c8c27c9d66415ad7cd8ef068c3d789819455e6
Amimaro <amir.zahlan@gmail.com>
2018-03-25 17:59:12 -0300

    Add color scheme to board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -10,7 +10,9 @@ class Board extends Component {
       renderedBoard: [],
       board: Array(parseInt(this.props.TOTAL_BLOCKS, 10)).fill(0),
       rows: this.props.TOTAL_BLOCKS / this.props.WIDTH * this.props.BLOCK_SIZE,
-      columns: this.props.TOTAL_BLOCKS / this.props.HEIGHT * this.props.BLOCK_SIZE
+      columns: this.props.TOTAL_BLOCKS / this.props.HEIGHT * this.props.BLOCK_SIZE,
+      isDark: false,
+      blocks: Array(40).fill('black'),
     }
     this.getRow = this.getRow.bind(this);
     this.getColumn = this.getColumn.bind(this);
@@ -21,8 +23,44 @@ class Board extends Component {
     this.createRect = this.createRect.bind(this);
     this.createRandomPath = this.createRandomPath.bind(this);
     this.createElements = this.createElements.bind(this);
+    this.toggleDarkness = this.toggleDarkness.bind(this);
   }
   componentWillMount() {
+    let blocks = this.state.blocks;
+
+    blocks[0] = 'LIGHTGRAY';
+    blocks[1] = 'WHITE';
+    blocks[2] = 'BLACK';
+
+    // Enemies
+    blocks[10] = 'LIGHTCORAL';
+    blocks[11] = 'RED';
+    blocks[12] = 'CRIMSON';
+    blocks[13] = 'FIREBRICK';
+    blocks[14] = 'DARKRED';
+    blocks[15] = 'INDIGO'; // Boss
+
+    // Heals
+    blocks[20] = 'PALEGREEN';
+    blocks[21] = 'MEDIUMSPRINGGREEN';
+    blocks[22] = 'MEDIUMSEAGREEN';
+    blocks[23] = 'FORESTGREEN';
+    blocks[24] = 'DARKGREEN';
+
+    // Weapons
+    blocks[30] = 'GRAY';
+    blocks[31] = 'DIMGRAY';
+    blocks[32] = 'LIGHTSLATEGRAY';
+    blocks[33] = 'SLATEGRAY';
+    blocks[34] = 'DARKSLATEGRAY';
+
+    // Player
+    blocks[40] = 'LIGHTSKYBLUE';
+    blocks[41] = 'DEEPSKYBLUE';
+    blocks[42] = 'DODGERBLUE';
+    blocks[43] = 'ROYALBLUE';
+    blocks[44] = 'MIDNIGHTBLUE';
+
     this.generateDungeons();
   }
   generateDungeons() {
@@ -191,6 +229,28 @@ class Board extends Component {
     });
     this.setState({board: board, renderedBoard: renderedBoard});
   }
+  toggleDarkness() {
+    if(this.state.isDark) {
+      console.log('isdark off');
+      for(let i = 0; i < this.props.TOTAL_BLOCKS; i++) {
+        let element =document.getElementById(i);
+        let elementValue = parseInt(element.getAttribute('value'));
+        if(elementValue < 40) {
+          element.style.backgroundColor = this.state.blocks[elementValue];
+        }
+      }
+    } else {
+      console.log('isdark on');
+      for(let i = 0; i < this.props.TOTAL_BLOCKS; i++) {
+        let element =document.getElementById(i);
+        let elementValue = parseInt(element.getAttribute('value'));
+        if(elementValue < 40) {
+          element.style.backgroundColor = this.state.blocks[2];
+        }
+      }
+    }
+    this.setState({isDark: !this.state.isDark});
+  }
   render() {
     return (
       <div>
@@ -206,7 +266,7 @@ class Board extends Component {
           <h4 style={{color: 'black'}}>Weapon: stick</h4>
           <h4 style={{color: 'red'}}>Atack: 7</h4>
           <p><Button>Restart</Button></p>
-          <p><Button>Toggle Darkness</Button></p>
+          <p><Button onClick={this.toggleDarkness}>Toggle Darkness</Button></p>
         </div>
       </div>
   );
