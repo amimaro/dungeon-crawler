b26c3238c263e04dba0657c855bc00404dfe15b5
Amimaro <amir.zahlan@gmail.com>
2018-03-25 10:39:31 -0300

    Remove unused functions

--- src/components/Listener/Listener.js
+++ src/components/Listener/Listener.js
@@ -7,14 +7,7 @@ class Listener extends Component {
 
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.move = this.move.bind(this);
-    this.isPlayerAround = this.isPlayerAround.bind(this);
-    this.isPlayer = this.isPlayer.bind(this);
-    this.isPath = this.isPath.bind(this);
-    this.setColor = this.setColor.bind(this);
-    this.getAttribute = this.getAttribute.bind(this);
-    this.setAttribute = this.setAttribute.bind(this);
     this.setValue = this.setValue.bind(this);
-    this.getIndex = this.getIndex.bind(this);
     this.getPlayer = this.getPlayer.bind(this);
     this.getId = this.getId.bind(this);
     this.getValue = this.getValue.bind(this);
@@ -27,12 +20,9 @@ class Listener extends Component {
     document.removeEventListener("keydown", this.handleKeyPress, false);
   }
   handleKeyPress = (event) => {
-    this.move(event.keyCode);
-  }
-  move(keyCode) {
     let step = 0;
     let index = this.getIndex();
-    switch (keyCode) {
+    switch (event.keyCode) {
       case 37:
         console.log('left pressed');
         step = -1;
@@ -53,38 +43,21 @@ class Listener extends Component {
     if (step != 0) {
       let player = this.getPlayer();
       let playerId = parseInt(this.getId(player));
-      if(this.getValue(this.getElementById(playerId + step)) == 1) {
-        console.log('path');
-      }
-      if(this.getValue(this.getElementById(playerId + step)) == 0) {
-        console.log('wall');
+      let nextElement = this.getElementById(playerId + step);
+      switch (this.getValue(nextElement)) {
+        case 0:
+          console.log('wall');
+          break;
+        case 1:
+          console.log('path');
+          this.move(player, nextElement);
+          break;
       }
     }
   }
-  isPlayer(index) {
-    if (this.getValue(index) >= 40 && this.getValue(index) <= 44)
-      return true;
-    return false;
-  }
-  isPath(index) {
-    if (this.getValue(index) == 1)
-      return true;
-    return false;
-  }
-  setColor(index, value) {
-    document.getElementById(index).style.backgroundColor = this.state.blocks[value];
-    this.setValue(index, value);
-  }
-  isPlayerAround(index) {
-    if (this.isPlayer(index - 1)) // Player at left
-      return index - 1;
-    if (this.isPlayer(index + 1)) // Player at right
-      return index + 1;
-    if (this.isPlayer(index - 100)) // Player up
-      return index - 100;
-    if (this.isPlayer(index + 100)) // Player down
-      return index + 100;
-    return 0;
+  move(origin, destination) {
+    origin.setValue(1);
+    destination.setValue(40);
   }
   getPlayer() {
     return document.querySelector('[value="40"]')
@@ -95,20 +68,12 @@ class Listener extends Component {
   getElementById(id) {
     return document.getElementById(id);
   }
-  getIndex() {
-    return this.props.index;
-  }
   getValue(element) {
     return element.getAttribute('value');
   }
-  setValue(index, value) {
-    this.setAttribute(index, 'value', value);
-  }
-  getAttribute(index, attr) {
-    return document.getElementById(index).getAttribute(attr);
-  }
-  setAttribute(index, attr, value) {
-    document.getElementById(index).setAttribute(attr, value);
+  setValue(element, value) {
+    element.setAttribute('value', value);
+    element.style.backgroundColor = this.state.blocks[value];
   }
   render() {
     return (<div></div>);
