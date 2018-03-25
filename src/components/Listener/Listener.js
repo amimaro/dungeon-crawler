546a92934280512c0c30b160d0547c6be58f8806
Amimaro <amir.zahlan@gmail.com>
2018-03-25 10:40:34 -0300

    Add color scheme to listener component

--- src/components/Listener/Listener.js
+++ src/components/Listener/Listener.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class Listener extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      blocks: Array(40).fill('black')
+    }
 
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.move = this.move.bind(this);
@@ -14,7 +16,43 @@ class Listener extends Component {
     this.getElementById = this.getElementById.bind(this);
   }
   componentWillMount() {
+    let blocks = this.state.blocks;
+
+    blocks[0] = 'LIGHTGRAY';
+    blocks[1] = 'WHITE';
+    blocks[2] = 'BLACK';
+
+    // Enemies
+    blocks[10] = 'LIGHTCORAL';
+    blocks[11] = 'CRIMSON';
+    blocks[12] = 'RED';
+    blocks[13] = 'FIREBRICK';
+    blocks[14] = 'DARKRED';
+    blocks[15] = 'INDIGO'; // Boss
+
+    // Heals
+    blocks[20] = 'PALEGREEN';
+    blocks[21] = 'MEDIUMSPRINGGREEN';
+    blocks[22] = 'MEDIUMSEAGREEN';
+    blocks[23] = 'FORESTGREEN';
+    blocks[24] = 'DARKGREEN';
+
+    // Weapons
+    blocks[30] = 'GRAY';
+    blocks[31] = 'DIMGRAY';
+    blocks[32] = 'LIGHTSLATEGRAY';
+    blocks[33] = 'SLATEGRAY';
+    blocks[34] = 'DARKSLATEGRAY';
+
+    // Player
+    blocks[40] = 'LIGHTSKYBLUE';
+    blocks[41] = 'DEEPSKYBLUE';
+    blocks[42] = 'DODGERBLUE';
+    blocks[43] = 'ROYALBLUE';
+    blocks[44] = 'MIDNIGHTBLUE';
+
     document.addEventListener("keydown", this.handleKeyPress, false);
+    this.setState({blocks: blocks, value: this.props.value})
   }
   componentWillUnmount() {
     document.removeEventListener("keydown", this.handleKeyPress, false);
