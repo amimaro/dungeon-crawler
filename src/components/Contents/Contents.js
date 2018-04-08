db9d4e604329c819c6a0679e87678d6e4a727eee
Amimaro <amir.zahlan@gmail.com>
2018-04-08 09:06:28 -0300

    Create contents data model :art:

---
+++ src/components/Contents/Contents.js
@@ -0,0 +1,40 @@
+class Contents {
+  getBlocks() {
+    let blocks = Array(40).fill('black');
+    blocks[0] = 'LIGHTGRAY'; // Wall
+    blocks[1] = 'WHITE'; // Path
+    blocks[2] = 'BLACK';
+
+    // Enemies
+    blocks[10] = 'LIGHTCORAL';
+    blocks[11] = 'RED';
+    blocks[12] = 'CRIMSON';
+    blocks[13] = 'FIREBRICK';
+    blocks[14] = 'DARKRED';
+    blocks[15] = 'INDIGO'; // Boss
+
+    // Heals
+    blocks[20] = 'PALEGREEN';
+    blocks[21] = 'MEDIUMSPRINGGREEN';
+    blocks[22] = 'MEDIUMSEAGREEN';
+    blocks[23] = 'FORESTGREEN';
+    blocks[24] = 'DARKGREEN';
+
+    // Weapons
+    blocks[30] = 'GRAY';
+    blocks[31] = 'DIMGRAY';
+    blocks[32] = 'LIGHTSLATEGRAY';
+    blocks[33] = 'SLATEGRAY';
+    blocks[34] = 'DARKSLATEGRAY';
+
+    // Player
+    blocks[40] = 'LIGHTSKYBLUE';
+    blocks[41] = 'DEEPSKYBLUE';
+    blocks[42] = 'DODGERBLUE';
+    blocks[43] = 'ROYALBLUE';
+    blocks[44] = 'MIDNIGHTBLUE';
+    return blocks;
+  }
+}
+
+export default Contents;
